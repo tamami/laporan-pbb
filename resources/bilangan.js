998e457dd36b3584aeb027a11f26c4e6ce72ffca
tamami <tamami.oka@gmail.com>
2016-03-25 14:50:55 +0700

    update function

--- resources/bilangan.js
+++ resources/bilangan.js
@@ -1,3 +1,7 @@
+function getHello() {
+  return "Hello brow!";
+}
+
 function toAbjad(angka) {
     temp = 0;
     abjad = " RUPIAH";
@@ -14,21 +18,88 @@ function toAbjad(angka) {
     if(angka >= 20 && angka < 100) {
         return prosesPuluhan(angka) + abjad;
     }
+
+    if(angka >= 100 && angka < 1000) {
+        return prosesRatusan(angka) + abjad;
+    }
+
+    if(angka >= 1000 && angka < 10000) {
+        return prosesRibuan(angka) + abjad;
+    }
+
+    if(angka >= 10000 && angka < 100000) {
+        return prosesPuluhanRibu(angka) + abjad;
+    }
 }    
+
+function prosesRatusanRibu(angka, tambahan) {
+  var tempRatusanRibu = Math.floor(angka / 1000);
+  var tempRibuan;
+  
+
+  
+  if(tambahan) {
+    return prosesRatusan(tempRatusanRibu, false) + " RIBU" + prosesRibuan(angka % 1000, true);
+  } else {
+    return (prosesRatusan(tempRatusanRibu, false) + " RIBU" + prosesRibuan(angka % 1000, true));
+  }
+}
+
+function prosesPuluhanRibu(angka, tambahan) {
+  var tempPuluhanRibu = Math.floor(angka / 1000);
+  var tempRatusan = angka % 1000;
+
+  if(tempRatusan >= 100) {
+  }
+}
+
+function prosesRibuan(angka, tambahan) {
+  var abjadLokal;
+  var temp = Math.floor(angka / 1000);
+  if(temp === 1) {
+    abjadLokal = " SERIBU";
+  } else if(temp > 1) {
+    abjadLokal = " " + prosesSatuan(temp) + " RIBU";
+  }
+
+  if(tambahan) 
+    return abjadLokal + prosesRatusan(angka % 1000, true);
+  else 
+    return (abjadLokal + prosesRatusan(angka % 1000, true)).trim();
+}
+
+function prosesRatusan(angka, tambahan) {
+  var temp;
+  var abjadLokal = "";
+  temp = Math.floor(angka / 100);
+  if(temp === 1) {
+    abjadLokal = " SERATUS";
+  } else if(temp > 1) {
+    abjadLokal = prosesSatuan(temp) + " RATUS";
+  } 
+  if(tambahan) 
+    return abjadLokal + prosesPuluhan(angka % 100, true);
+  else  
+    return (abjadLokal + prosesPuluhan(angka % 100, true)).trim();
+}
     
-function prosesPuluhan(angka) {
-    int temp = angka / 10;
+function prosesPuluhan(angka,tambahan) {
+    var temp = Math.floor(angka / 10);
+    var abjadLokal;
     if(temp  > 1) {
       abjadLokal = " " + getHuruf(temp) + " PULUH" + prosesSatuan(angka % 10);
     } else {
       abjadLokal = prosesSatuan(angka);
     }
-    return abjadLokal;
+    if(tambahan)
+      return abjadLokal;
+    else
+      return (abjadLokal).trim();
 }
 
 function prosesSatuan(angka) {
   if(angka >= 1) {
-    return " " + getHuruf(angka);
+    return getHuruf(angka);
   } else {
     return "";
   }
